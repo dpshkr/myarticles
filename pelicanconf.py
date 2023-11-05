b2917605115c08ebf3c2cbdf4d2abc71e186210b
Pushkar <121485926+dpshkr@users.noreply.github.com>
2023-11-05 23:58:08 +0530

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -27,7 +27,7 @@ SOCIAL = (('You can add links in your config file', '#'),
 
 DEFAULT_PAGINATION = 10
 THEME = "sakura-pelican"
-LICENSE = "CC-BY"
+LICENSE = "CC0"
 
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
